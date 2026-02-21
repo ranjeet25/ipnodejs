9f907deb94be3f99eef1f8c610d0f3b859341dc7
unknown <sawranjeet550@gmail.com>
2026-02-21 18:24:45 +0530

    added initial files

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+const products = [
+  { id: 1, name: 'Product 1', price: 10 },
+  { id: 2, name: 'Product 2', price: 20 },
+  { id: 3, name: 'Product 3', price: 30 },
+];
+
+const product123 = { id: 123, name: 'Product 123', price: 123 };
+
+// Get all products
+app.get('/products', (req, res) => {
+
+    // get query parameter sort
+    const sort = req.query.sort;
+    console.log(sort);
+    res.send(products);
+}               
+);
+
+// Dynamic route - path parameter
+app.get('/products/:id', (req, res) => {
+
+ // get path parameter id
+  const id = req.params.id; 
+  console.log(id);
+
+  if (id == 123) {
+    res.send(product123);
+  } else {
+    res.send({ message: 'Product not found' });
+  } 
+}               
+);
+
+
+
+
+
+
+
+app.post('/login', (req, res) => {
+  res.send('this is login route');
+}       
+);
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
